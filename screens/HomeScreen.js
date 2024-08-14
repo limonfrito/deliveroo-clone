62e57ec56f33b9daf2d4cca39005e6c18bbaf5fa
Salvador Garcia <chasalva7565@hotmail.com>
2024-08-14 02:50:59 -0600

    Header y barra de busqueda terminados

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,12 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon as SearchIcon,
+  AdjustmentsVerticalIcon as AdjustmenstIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -12,19 +18,34 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       {/* Header */}
-      <View className="flex-row">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{
             uri: "https://links.papareact.com/wru",
           }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
-          <Text>Deliver Now!</Text>
-          <Text>Current Location</Text>
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <View className="flex-row items-end gap-1">
+            <Text className="font-bold text-xl">Current Location</Text>
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </View>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 bg-gray-200 flex-1 p-3 items-center">
+          <SearchIcon color="gray" size={20}/>
+          <TextInput placeholder="Restaurants and cuisines" keyboardType="default" />
+        </View>
+
+        <AdjustmenstIcon color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
