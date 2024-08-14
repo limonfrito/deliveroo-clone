c6a9efa79907e068faa303b723d120e02d3bfaa0
Salvador Garcia <chasalva7565@hotmail.com>
2024-08-13 20:25:48 -0600

    HomeScreen agregado

---
+++ screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView>
+      {/* Header */}
+      <View className="flex-row">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View>
+          <Text>Deliver Now!</Text>
+          <Text>Current Location</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
