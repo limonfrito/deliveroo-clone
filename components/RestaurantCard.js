fd487305e0061546df67328e795e2cdcade92979
Salvador Garcia <chasalva7565@hotmail.com>
2024-08-22 02:23:52 -0600

    version de expo actualizada y avance en featured rows

---
+++ components/RestaurantCard.js
@@ -0,0 +1,31 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { StarIcon } from "react-native-heroicons/solid";
+import { MapPinIcon } from "react-native-heroicons/outline";
+
+const RestaurantCard = ({
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  short_description,
+  dishes,
+  logn,
+  lat,
+}) => {
+  return (
+    <TouchableOpacity>
+      <Image source={{ uri: imgUrl }} className="h-36 w-64 rounded-sm " />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+            <StarIcon color={'green'} opacity={0.5} size={22} />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default RestaurantCard;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -8,6 +15,7 @@ import {
   AdjustmentsVerticalIcon as AdjustmenstIcon,
 } from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -38,12 +46,14 @@ const HomeScreen = () => {
         <UserIcon size={35} color="#00CCBB" />
       </View>
 
-
       {/* Search */}
       <View className="flex-row items-center space-x-2 pb-2 mx-4">
         <View className="flex-row space-x-2 bg-gray-200 flex-1 p-3 items-center">
-          <SearchIcon color="gray" size={20}/>
-          <TextInput placeholder="Restaurants and cuisines" keyboardType="default" />
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
         </View>
 
         <AdjustmenstIcon color="#00CCBB" />
@@ -54,8 +64,26 @@ const HomeScreen = () => {
         {/* Categories */}
         <Categories />
 
+        {/* Featured */}
+        <FeaturedRow
+          id="abba-1"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
 
-        {/* Featured Rows */}
+        {/* Tasty Discounts */}
+        <FeaturedRow
+          id="abba-2"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+
+        {/* Offers Near You */}
+        <FeaturedRow
+          id="abba-3"
+          title="Offers Near You"
+          description="Why not support your local restaurant tonight!"
+        />
       </ScrollView>
     </SafeAreaView>
   );
